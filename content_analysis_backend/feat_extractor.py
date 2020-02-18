b4c39ed1fb28730df8519cfe439ee3c20f995659
isspek <ipekbrs@gmail.com>
2020-02-18 12:24:30 +0100

    added tokenization

--- content_analysis_backend/feat_extractor.py
+++ content_analysis_backend/feat_extractor.py
@@ -1,4 +1,5 @@
-from nltk import word_tokenize
+from nltk.tokenize import TweetTokenizer
+from nltk.tag import pos_tag
 '''
 This package contains content features for measuring the credibility.
 generic credibility features proposed by Olteanu et al. [2013]
@@ -25,9 +26,11 @@ generic credibility features proposed by Olteanu et al. [2013]
 '''
 
 
-class GenericContentFeats:
+class ContentFeats:
     def __init__(self, text):
         self.text = text
+
+        # ====== Content feats proposed by Olteanu et al. [2013] ======
         self.num_exclamations = None
         self.num_commas = None
         self.num_dots = None
@@ -49,12 +52,15 @@ class GenericContentFeats:
         self.num_RB = None
         self.num_DT = None
 
-    def compute(self):
+    def compute(self, tokenizer=TweetTokenizer()):
         self.num_exclamations = count_exclamations(self.text)
         self.num_commas = count_commas(self.text)
         self.num_dots = count_dots(self.text)
         self.num_questions = count_questions(self.text)
-        self.num_tokens = count_tokens(self.text)
+
+        # split text into tokens, default tokenizer is TweetTokenizer
+        tokens = tokenizer.tokenize(self.text)
+        self.num_tokens = len(tokens)
 
 
 def count_exclamations(text):
@@ -71,9 +77,3 @@ def count_dots(text):
 
 def count_questions(text):
     return sum(1 for char in text if char == '?')
-
-
-def count_tokens(text):
-    return len(word_tokenize(text))
-
-
